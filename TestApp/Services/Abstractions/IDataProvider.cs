f79532fcef5f366dad2a4c799c84ed99015152a8
Ivan <kushnerevin@gmail.com>
2023-06-14 15:32:21 +0300

    Added DataProvider service

---
+++ TestApp/Services/Abstractions/IDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestApp.Models;
+
+namespace TestApp.Services.Abstractions
+{
+    public interface IDataProvider
+    {
+        Task<IEnumerable<Order>> GetOrdersAsync();
+    }
+}
---
+++ TestApp/Services/Implementations/DataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApp.Models;
+using TestApp.Services.Abstractions;
+
+namespace TestApp.Services.Implementations
+{
+    internal class DataProvider : IDataProvider
+    {
+        public async Task<IEnumerable<Order>> GetOrdersAsync()
+        {
+            using (TestDBEntities testDBEntities = new TestDBEntities())
+            {
+                List<Order> orders;
+                orders = await testDBEntities.Orders.Include(o => o.User)
+                    .Include(o => o.Order_lines.Select(l => l.Product))                    
+                    .ToListAsync();
+                return orders;
+            }
+        }
+    }
+}
